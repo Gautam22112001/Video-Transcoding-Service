840fb2650cfa0f0214d493cb8be9c69c031bc6a1
Gautam22112001 <71265763+Gautam22112001@users.noreply.github.com>
2024-08-14 17:44:39 +0530

    first commit

---
+++ container/index.js
@@ -0,0 +1,72 @@
+import {
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import * as fs from "node:fs/promises";
+import * as fsOld from "node:fs";
+import path from "node:path";
+import ffmpeg from "fluent-ffmpeg";
+
+const s3Client = new S3Client({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: "AKIA2CCOQSE5VOLZ3MOD",
+    secretAccessKey: "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",
+  },
+});
+
+//.ENV
+const BUCKET_NAME = process.env.BUCKET_NAME;
+const KEY = process.env.KEY;
+
+//Download the raw video
+async function init() {
+  const command = new GetObjectCommand({
+    Bucket: BUCKET_NAME,
+    Key: KEY,
+  });
+
+  const result = await s3Client.send(command);
+  const originalFilePath = `original-video.mp4`;
+  await fs.writeFile(originalFilePath, result.Body);
+
+  const originalVideoPath = path.resolve(originalFilePath);
+
+  // Start the transcoder
+  const RESOLUTIONS = [
+    { name: "360p", width: 480, height: 360 },
+    { name: "480p", width: 858, height: 480 },
+  ];
+
+  const promises = RESOLUTIONS.map((resolution) => {
+    const output = `video-${resolution.name}.mp4`;
+
+    return new Promise((resolve) => {
+      ffmpeg(originalVideoPath)
+        .output(output)
+        .withVideoCodec("libx264")
+        .withAudioCodec("aac")
+        .withSize(`${resolution.width}x${resolution.height}`)
+        .on("start", () =>
+          console.log("Start", `${resolution.width}x${resolution.height}`)
+        )
+        .on("end", async () => {
+          // Upload the video
+          const putCommand = new PutObjectCommand({
+            Bucket: "gautammanocha22112001-video-output",
+            Key: output,
+            Body: fsOld.createReadStream(path.resolve(output)),
+          });
+          await s3Client.send(putCommand);
+          console.log(`uploaded: ${output}`);
+          resolve();
+        })
+        .format("mp4")
+        .run();
+    });
+  });
+  await Promise.all(promises);
+}
+
+init();
